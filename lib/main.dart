87113a0f1c11229bbd6e7d9afaf8d620da6e1f21
JerinFrancisA <jerinfrancis77@gmail.com>
2019-08-22 13:45:16 +0530

    Updated app widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,121 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(child: Text('Dicee')),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: MyDice(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class MyDice extends StatefulWidget {
+  @override
+  _MyDiceState createState() => _MyDiceState();
+}
+
+class _MyDiceState extends State<MyDice> {
+  int lnum = 1;
+  int rnum = 1;
+  String oe = 'EVEN';
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: FlatButton(
+                      onPressed: () {
+                        change();
+                      },
+                      child: Image.asset('images/dice$lnum.png'),
+                    ),
+                  ),
+                  Expanded(
+                    child: FlatButton(
+                      onPressed: () {
+                        change();
+                      },
+                      child: Image.asset('images/dice$rnum.png'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              'SUM = ${lnum + rnum} ($oe)',
+              style: TextStyle(
+                color: Colors.amber,
+                fontFamily: 'FredokaOne',
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.5,
+              ),
+            ),
+            SizedBox(
+              height: 40.0,
+            ),
+            Card(
+              child: FlatButton(
+                splashColor: Colors.red,
+                color: Colors.white,
+                onPressed: () {
+                  change();
+                },
+                child: IconButton(
+                  iconSize: 40.0,
+                  color: Colors.red,
+                  icon: Icon(Icons.call_missed_outgoing),
+                  onPressed: () {
+                    change();
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 40.0,
+            ),
+            Text(
+              "ROLL",
+              style: TextStyle(
+                color: Colors.cyan,
+                fontFamily: 'FredokaOne',
+                fontSize: 50.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 3.0,
+              ),
+            ),
+            SizedBox(
+              height: 40.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void change() {
+    setState(() {
+      lnum = Random().nextInt(6) + 1;
+      rnum = Random().nextInt(6) + 1;
+      if ((lnum + rnum) % 2 == 0) {
+        oe = "EVEN";
+      } else {
+        oe = "ODD";
+      }
+    });
   }
 }
